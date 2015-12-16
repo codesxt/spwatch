57dfee0152b2ca414a5e7509230f5d7c8cd02f1d
Bruno Faúndez Valenzuela <gameover_sxt_nah@hotmail.com>
2015-12-16 12:57:11 -0300

    It's showing last ten minutes of water consumption (and looks pretty).

--- src/client/js/ClientApi.js
+++ src/client/js/ClientApi.js
@@ -7,12 +7,12 @@ var restapi = express();
 
 console.log("Registering endpoint: /ten_last_minutes.json");
 restapi.get('/ten_last_minutes.json', function(req, res){
-  console.log("RECEIVED GET REQUEST.");
+  //console.log("RECEIVED GET REQUEST.");
   /*
   db.get("SELECT value FROM sensordata ORDER BY year, month, day, hour, minute ASC LIMIT 10", function(err, row){
       res.json({ "data" : row.value });
   });*/
-  var query = "SELECT value FROM sensordata ORDER BY year, month, day, hour, minute ASC LIMIT 10";
+  var query = "SELECT value FROM sensordata ORDER BY year DESC, month DESC, day DESC, hour DESC, minute DESC LIMIT 10";
   var values = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
   db.all(query, function(err, rows) {
@@ -21,8 +21,9 @@ restapi.get('/ten_last_minutes.json', function(req, res){
       //values.push(rows[i].value);
       values[i] = rows[i].value;
     }
-    res.json('callback('+ JSON.stringify(values) + ');');
-    //res.jsonp({"data": values});
+    values.reverse();
+    //res.json('callback('+ JSON.stringify(values) + ');');
+    res.jsonp({"data": values});
   });
 });
 
--- src/client/js/client.js
+++ src/client/js/client.js
@@ -1,14 +1,14 @@
 var ctx = document.getElementById("data-chart").getContext("2d");
 
-var initialData = getTenRandomValues();
+var initialData = [0,0,0,0,0,0,0,0,0,0];
 
 var data = {
     labels: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
     datasets: [
         {
             label: "My First dataset",
-            fillColor: "rgba(220,220,220,0.2)",
-            strokeColor: "rgba(220,220,220,1)",
+            fillColor: "rgba(87,213,255,0.2)",
+            strokeColor: "rgba(66,168,200,1)",
             pointColor: "rgba(220,220,220,1)",
             pointStrokeColor: "#fff",
             pointHighlightFill: "#fff",
@@ -18,45 +18,11 @@ var data = {
     ]
 };
 
-var dataChart = new Chart(ctx).Bar(data);
+var dataChart = new Chart(ctx).Line(data);
 
 $('#mainChart').on('click', function(event) {
   event.preventDefault();
-  //var new_data = getTenRandomValues();
-  var new_data = [];
-  /*
-  console.log("Before.");
-  $.getJSON('localhost:3000/ten_last_minutes.json', function(data){
-    console.log("Inside.");
-    console.log(data);
-  });
-  console.log("After.");
-
-  for(var i = 0; i < new_data.length; i++){
-    dataChart.datasets[0].bars[i].value = new_data[i];
-  }
-  dataChart.update();*/
-
-  $.ajax({
-    url: "localhost:3000/ten_last_minutes.json",
-
-    // The name of the callback parameter, as specified by the YQL service
-    jsonp: "callback",
-
-    // Tell jQuery we're expecting JSONP
-    dataType: "jsonp",
-
-    // Tell YQL what we want and that we want JSON
-    data: {
-        q: "select title,abstract,url from search.news where query=\"cat\"",
-        format: "json"
-    },
-
-    // Work with the response
-    success: function( response ) {
-        console.log( response ); // server response
-    }
-});
+  updateLastTenMinutes();
 });
 
 function getTenRandomValues(){
@@ -68,12 +34,23 @@ function getTenRandomValues(){
   return arr;
 }
 
-/*
+function updateLastTenMinutes(){
+  var new_data = [];
+  $.ajax({
+      url: 'http://localhost:3000/ten_last_minutes.json',
+      dataType: 'jsonp',
+      success: function(dataWeGotViaJsonp){
+          var text = '';
+          var len = dataWeGotViaJsonp.length;
+          new_data = dataWeGotViaJsonp.data;
+          for(var i = 0; i < new_data.length; i++){
+            dataChart.datasets[0].points[i].value = new_data[i];
+          }
+          dataChart.update();
+      }
+  });
+}
+
 setInterval(function(){
-  var new_data = getTenLastMinutes();
-  for(var i = 0; i < new_data.length; i++){
-    dataChart.datasets[0].bars[i].value = new_data[i];
-  }
-  dataChart.update();
+  updateLastTenMinutes();
 }, 1000);
-*/
