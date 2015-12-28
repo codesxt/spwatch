1a25eed8899f8e5a4cc5b6ce6364c8d0fddeaf9f
Bruno Faúndez Valenzuela <gameover_sxt_nah@hotmail.com>
2015-12-28 15:05:50 -0300

    Add last24hours and last7days.

--- src/client/js/ClientApi.js
+++ src/client/js/ClientApi.js
@@ -27,4 +27,48 @@ restapi.get('/ten_last_minutes.json', function(req, res){
   });
 });
 
+console.log("Registering endpoint: /tfour_last_hours.json");
+restapi.get('/tfour_last_hours.json', function(req, res){
+  //console.log("RECEIVED GET REQUEST.");
+  /*
+  db.get("SELECT value FROM sensordata ORDER BY year, month, day, hour, minute ASC LIMIT 10", function(err, row){
+      res.json({ "data" : row.value });
+  });*/
+  var query = "SELECT sum(value) AS value FROM sensordata GROUP BY year, month, day, hour ORDER BY year DESC, month DESC, day DESC, hour DESC LIMIT 24";
+  var values = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+  db.all(query, function(err, rows) {
+    for(var i=0; i<rows.length; i++){
+      //console.log(rows[i].value);
+      //values.push(rows[i].value);
+      values[i] = rows[i].value;
+    }
+    values.reverse();
+    //res.json('callback('+ JSON.stringify(values) + ');');
+    res.jsonp({"data": values});
+  });
+});
+
+console.log("Registering endpoint: /seven_last_days.json");
+restapi.get('/seven_last_days.json', function(req, res){
+  //console.log("RECEIVED GET REQUEST.");
+  /*
+  db.get("SELECT value FROM sensordata ORDER BY year, month, day, hour, minute ASC LIMIT 10", function(err, row){
+      res.json({ "data" : row.value });
+  });*/
+  var query = "SELECT sum(value) AS value FROM sensordata GROUP BY year, month, day ORDER BY year DESC, month DESC, day DESC, hour DESC LIMIT 7";
+  var values = [0, 0, 0, 0, 0, 0, 0];
+
+  db.all(query, function(err, rows) {
+    for(var i=0; i<rows.length; i++){
+      //console.log(rows[i].value);
+      //values.push(rows[i].value);
+      values[i] = rows[i].value;
+    }
+    values.reverse();
+    //res.json('callback('+ JSON.stringify(values) + ');');
+    res.jsonp({"data": values});
+  });
+});
+
 restapi.listen(3000);
--- src/client/js/client.js
+++ src/client/js/client.js
@@ -2,6 +2,13 @@ var ctx = document.getElementById("data-chart").getContext("2d");
 
 var initialData = [0,0,0,0,0,0,0,0,0,0];
 
+var updateRate = 1000;
+
+var screenChart = 0;
+//0: last ten minutes
+//1: last 24 hours
+//2: last 7 days
+
 var data = {
     labels: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
     datasets: [
@@ -18,11 +25,27 @@ var data = {
     ]
 };
 
-var dataChart = new Chart(ctx).Line(data);
+var dataChart = new Chart(ctx).Line(data, {animation : false});
 
-$('#mainChart').on('click', function(event) {
+$('#minutelyChart').on('click', function(event) {
   event.preventDefault();
+  updateRate = 1000;
   updateLastTenMinutes();
+  screenChart = 0;
+});
+
+$('#hourlyChart').on('click', function(event) {
+  event.preventDefault();
+  updateRate = 60*1000;
+  updateLast24Hours();
+  screenChart = 1;
+});
+
+$('#dailyChart').on('click', function(event) {
+  event.preventDefault();
+  updateRate = 60*60*1000;
+  updateLast7Days();
+  screenChart = 2;
 });
 
 function getTenRandomValues(){
@@ -40,17 +63,76 @@ function updateLastTenMinutes(){
       url: 'http://localhost:3000/ten_last_minutes.json',
       dataType: 'jsonp',
       success: function(dataWeGotViaJsonp){
-          var text = '';
-          var len = dataWeGotViaJsonp.length;
           new_data = dataWeGotViaJsonp.data;
+          clearChart();
+          var d = new Date();
           for(var i = 0; i < new_data.length; i++){
-            dataChart.datasets[0].points[i].value = new_data[i];
+            //dataChart.datasets[0].points[i].value = new_data[i];
+            //dataChart.addData([new_data[i]], (hour%60)+":"+(min-(10-i)%60));
+            nd = new Date(d - (10-i)*60000);
+            dataChart.addData([new_data[i]], nd.getHours()+":"+nd.getMinutes());
           }
           dataChart.update();
       }
   });
 }
 
-setInterval(function(){
-  updateLastTenMinutes();
-}, 1000);
+function updateLast24Hours(){
+  var new_data = [];
+  $.ajax({
+      url: 'http://localhost:3000/tfour_last_hours.json',
+      dataType: 'jsonp',
+      success: function(dataWeGotViaJsonp){
+          new_data = dataWeGotViaJsonp.data;
+          clearChart();
+          var d = new Date();
+          for(var i = 0; i < new_data.length; i++){
+            //dataChart.datasets[0].points[i].value = new_data[i];
+            nd = new Date(d - (24-i)*(60*60*1000));
+            dataChart.addData([new_data[i]], "("+nd.getUTCDate()+")"+nd.getHours()+":00");
+          }
+          dataChart.update();
+      }
+  });
+}
+
+function updateLast7Days(){
+  var new_data = [];
+  $.ajax({
+      url: 'http://localhost:3000/seven_last_days.json',
+      dataType: 'jsonp',
+      success: function(dataWeGotViaJsonp){
+          new_data = dataWeGotViaJsonp.data;
+          clearChart();
+          var d = new Date();
+          for(var i = 0; i < new_data.length; i++){
+            //dataChart.datasets[0].points[i].value = new_data[i];
+            nd = new Date(d - (7-i)*(24*60*60*1000));
+            dataChart.addData([new_data[i]], nd.getUTCDate()+1);
+          }
+          dataChart.update();
+      }
+  });
+}
+
+function clearChart(){
+  //console.log("Tamaño del dataset: "+dataChart.datasets[0].points.length);
+  var nData = dataChart.datasets[0].points.length;
+  for(var i = 0; i < nData; i++){
+    dataChart.removeData();
+  }
+}
+
+var intervalUpdater = function(){
+  if(screenChart == 0){
+    updateLastTenMinutes();
+  }else if(screenChart == 1){
+    updateLast24Hours();
+  }else if(screenChart == 2){
+    updateLast7Days();
+  }
+  clearInterval(interval);
+  interval = setInterval(intervalUpdater, updateRate);
+}
+
+var interval = setInterval(intervalUpdater, updateRate);
